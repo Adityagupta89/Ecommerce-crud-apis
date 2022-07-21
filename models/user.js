33fe82e355803c6a802add65d068bc9212fede9c
Aditya <aditya.gupta@thinkitive.com>
2022-07-21 18:59:38 +0530

    Update Schema

--- models/user.js
+++ models/user.js
@@ -1,26 +1,27 @@
+const Joi = require('joi');
 const mongoose=require('mongoose');
 const User=mongoose.model('User',new mongoose.Schema({
     first_name:{
         type:String,
         required:true,
-        minlength:5,
+        minlength:2,
         maxlength:50
     },
     last_name:{
         type:String,
         required:true,
-        minlength:5,
+        minlength:2,
         maxlength:50
     },
     email:{
         type:String,
         required:true,
-        unique:true
+        
     },
     mobile_no:{
         type:Number,
         required:true,
-        unique:true
+    
     },
     address:{
         type:String,
@@ -33,4 +34,17 @@ const User=mongoose.model('User',new mongoose.Schema({
         default:Date.now
     }
 }))
+function validateUser(user) {
+    const schema = Joi.object().keys({
+      first_name: Joi.string().min(2).max(50).required(),
+      last_name: Joi.string().min(2).max(50).required(),
+      email: Joi.string().required().email(),
+      mobile_no:Joi.number().required(),
+      address:Joi.string().min(5).max(100).required(),
+      dob:Joi.date().required()
+    });
+    return schema.validate(user);
+  }
+
 module.exports.User=User
+module.exports.validateUser=validateUser
