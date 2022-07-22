fb03b9b74fa139308055c2feaa3adcda369884f8
Aditya <aditya.gupta@thinkitive.com>
2022-07-22 13:07:32 +0530

    add : user-controller

---
+++ controllers/user-controller.js
@@ -0,0 +1,77 @@
+const mongoose = require("mongoose");
+const { User, validateUser } = require("../models/user");
+const getUser=async (req, res) => {
+    const users = await User.find();
+    res.send(users);
+ }
+ 
+ const getUserById=async (req, res) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id))
+      return res.status(400).send("Id is not valid");
+    const user = await User.findById(req.params.id);
+    if (!user)
+      return res
+        .status(404)
+        .send("The customer with the given ID was not found.");
+    res.send(user);
+  }
+
+  const createUser=async (req, res) => {
+    const { error } = validateUser(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    let user = new User({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      mobile_no: req.body.mobile_no,
+      address: req.body.address,
+      dob: req.body.dob,
+    });
+    try {
+      user = await user.save();
+    } catch (err) {
+      return res.status(400).send(err.message);
+    }
+    res.status(201).send(user);
+  }
+
+  const updateUser=async (req, res) => {
+    const { error } = validateUser(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    if (!mongoose.Types.ObjectId.isValid(req.params.id))
+      return res.status(400).send("Id is not valid");
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        mobile_no: req.body.mobile_no,
+        address: req.body.address,
+        dob: req.body.dob,
+      });
+      if (!user)
+        return res
+          .status(404)
+          .send("The customer with the given ID was not found.");
+  
+      res.send(user);
+    } catch (err) {
+      res.status(400).send("err.message");
+    }
+  }
+  const deleteUser=async (req, res) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id))
+      return res.status(400).send("Id is not valid");
+    const user = await User.findByIdAndRemove(req.params.id);
+    if (!User)
+      return res
+        .status(204)
+        .send("The customer with the given ID was not found.");
+  
+    res.send("User deleted");
+  }
+  module.exports.getUser=getUser
+  module.exports.getUserById=getUserById
+  module.exports.createUser=createUser
+  module.exports.updateUser=updateUser
+  module.exports.deleteUser=deleteUser
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express=require('express');
 const app=express();
 const mongoose=require('mongoose')
-const user_routes=require('./routes/users')
+const user_routes=
+require('./routes/users')
 app.use(express.json())
 app.use('/api/user/',user_routes)
-const db_url =
-  "mongodb+srv://aditya_gupta89:1234@cluster0.8r6bxyw.mongodb.net/?retryWrites=true&w=majority";
-mongoose.connect(db_url)
+const {DB,PORT}=require('./config/index')
+mongoose.connect(DB)
   .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...'));
-let port =process.env.Port||3000
-app.listen(port,()=>console.log(`Listening to the port - ${port} `))
+  .catch(err => console.log(err));
+
+app.listen(PORT,()=>console.log(`Listening to the port - ${PORT} `))
